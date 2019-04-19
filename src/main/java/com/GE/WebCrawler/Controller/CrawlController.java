14f7cbb04d87c808b809ba3d29e9d8727f469bb8
patel-kp-github <mail.patel.k@gmail.com>
2019-04-19 10:48:40 +0530

    some changes

--- src/main/java/com/GE/WebCrawler/Controller/CrawlController.java
+++ src/main/java/com/GE/WebCrawler/Controller/CrawlController.java
@@ -4,10 +4,8 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.GE.WebCrawler.service.interfaces.IWebPageProcessor;
@@ -18,22 +16,10 @@ public class CrawlController {
 
 	@Autowired
 	IWebPageProcessor process;
-	
-	@RequestMapping("/hello")
-	public String helloMethod() {
-		System.out.println("inside hello..........");
-		return "HELLO GE Bangalore ...!!!!";
-	}
 
 	@RequestMapping("/parse")
 	public Map<String, List<String>> crawlController(@RequestBody Map<String, List> internetInput) {
 		return process.processRequestPages(internetInput);
-	}
-
-	@RequestMapping("/parse/URL")
-	public Map<String, List<String>> crawlController(@RequestParam("URL") String URL) {
-		System.out.println("crawlController  URL.........."+URL);
-		return process.processSinglePage(URL);
 	}
 
 }
--- src/main/java/com/GE/WebCrawler/service/impl/HttpBaseApiServiceImpl.java
+++ src/main/java/com/GE/WebCrawler/service/impl/HttpBaseApiServiceImpl.java
@@ -1,52 +1,27 @@
 package com.GE.WebCrawler.service.impl;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.GE.WebCrawler.service.interfaces.IHttpBaseApiService;
 
+@Service
 public class HttpBaseApiServiceImpl implements IHttpBaseApiService {
 
 	@Override
-	public boolean HttpRequestHandler(String URL) {
-		// TODO Auto-generated method stub
-		DefaultHttpClient client = new DefaultHttpClient();
-		HttpGet httpget = new HttpGet(URL);
-		HttpResponse response = null;
-		System.out.println("code 1: ");
-		try {
-			response = client.execute(httpget);
-			System.out.println("code 2: " + response);
-			int code = response.getStatusLine().getStatusCode();
-			System.out.println("code: " + code);
-			if (code / 100 != 2)
-				return false;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-		return true;
-	}
-
-	@Override
 	public boolean IsValidPage(String URL) {
-		DefaultHttpClient client = new DefaultHttpClient();
-		HttpGet httpget = new HttpGet(URL);
-		HttpResponse response = null;
-		System.out.println("code 1: ");
 		try {
-			response = client.execute(httpget);
-			System.out.println("code 2: " + response);
-			int code = response.getStatusLine().getStatusCode();
-			System.out.println("code: " + code);
-			if (code / 100 != 2)
-				return false;
+			RestTemplate restTemplate = new RestTemplate();
+			HttpStatus responseEntity = restTemplate.exchange(URL, HttpMethod.GET, null, String.class).getStatusCode();
+			if (responseEntity.OK == HttpStatus.OK) {
+				return true;
+			}
 		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
+
 		}
-		return true;
+		return false;
 	}
 
 }
